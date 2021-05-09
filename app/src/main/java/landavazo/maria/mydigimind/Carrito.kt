52630e93be45cc92456832383761339b8a79fe00
MariaSofi <maria.landavazo@potros.itson.edu.mx>
2021-05-09 14:02:29 -0700

    git commit

---
+++ app/src/main/java/landavazo/maria/mydigimind/Carrito.kt
@@ -0,0 +1,9 @@
+package landavazo.maria.mydigimind
+import java.io.Serializable
+
+class Carrito: Serializable {
+    var recordatorios =ArrayList<Recordatorio>()
+    fun agregar(p:Recordatorio):Boolean{
+        return recordatorios.add(p)
+    }
+}
